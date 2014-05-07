6f2db1f09209f282c3a71eba22633212a71ea463
nigonro <nicolas.gonzalezr@gmail.com>
2014-05-07 00:30:00 -0300

    Drag & Drop replacement new json2.js for old browsers (still to be added in code) sync2.js - new version of sync algorithm

---
+++ js/sync2.js
@@ -0,0 +1,16 @@
+function Machine () {
+    this.name = '';
+    this.epo = '';
+    this.ad = '';
+    this.altiris = '';
+    this.status = '';
+    this.class = '';
+    this.aging = '';
+
+    this.toJSON = toJSON;
+
+    function toJSON() {
+        
+    }
+}
+
